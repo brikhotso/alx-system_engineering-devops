be5c23f172669c5d2180ee42308fcd89e11a4ba8
brikhotso <brenda.rikhotso@gmail.com>
2024-03-26 16:03:51 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,14 @@
 # Puppet manifest to install nginx with Nginx redirection
+
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
+file_line { 'nginx_redirection':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => "rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
 }
 
 file { '/var/www/html/index.html':
