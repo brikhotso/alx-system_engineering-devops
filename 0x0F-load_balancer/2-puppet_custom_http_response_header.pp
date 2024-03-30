766991cb5f0937ba344f950bed73af33743853b7
brikhotso <brenda.rikhotso@gmail.com>
2024-03-29 22:28:48 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,21 +4,19 @@ package { 'nginx':
   ensure => installed,
 }
 
-nginx::resource::vhost { 'default':
-  ensure       => present,
-  www_root     => '/var/www/html',
-  index_files  => ['index.html'],
-  listen_port  => '80',
-  server_name  => 'localhost',
-  rewrite      => {
-    'redirect_me' => {
-      'from' => '^/redirect_me',
-      'to'   => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
-    },
-  },
-  custom_config => {
-    'add_header X-Served-By' => $facts['hostname'],
-  },
+file_line { 'nginx_redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => "rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
+}
+
+file_line { 'header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $HOSTNAME;',
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/index.html':
