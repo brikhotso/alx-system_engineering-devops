ef2370d3473052b9361d1c4100b0b4ec8217bc8d
brikhotso <brenda.rikhotso@gmail.com>
2024-03-29 23:46:34 -0700

    http header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,2 +1,16 @@
 # Puppet manifest to install nginx with custom HTTP header
 
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
