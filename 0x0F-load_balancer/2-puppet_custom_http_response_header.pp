8e995f1dae1c0d823534024b3ed327654c29aa84
brikhotso <brenda.rikhotso@gmail.com>
2024-03-29 20:41:41 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,32 @@
-# Puppet manifest creating a custom HTTP header response
+# Puppet manifest to install nginx with custom HTTP header
+
+package { 'nginx':
+  ensure => installed,
+}
+
+nginx::resource::vhost { 'default':
+  ensure       => present,
+  www_root     => '/var/www/html',
+  index_files  => ['index.html'],
+  listen_port  => '80',
+  server_name  => 'localhost',
+  rewrite      => {
+    'redirect_me' => {
+      'from' => '^/redirect_me',
+      'to'   => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
+    },
+  },
+  custom_config => {
+    'add_header X-Served-By' => '$hostname',
+  },
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
