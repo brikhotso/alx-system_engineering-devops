281640b19922d5e7665723809cdf015cadec552c
brikhotso <brenda.rikhotso@gmail.com>
2024-05-07 11:39:02 -0700

    documentation and formating

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,19 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ return number of subsbribers """
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers)
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+             Returns 0 if the subreddit doesn't exist.
+    """
+    headers = {'User-Agent': 'linux:0x16.api.advanced'}
+    response = requests.get('https://www.reddit.com/r/{}/about.json'
+                            .format(subreddit), headers=headers)
 
     if response.status_code == 200:
         return response.json()['data']['subscribers']
