45f345e74e8445447659ce17f6ab9364547ef6d9
brikhotso <brenda.rikhotso@gmail.com>
2024-05-07 12:33:41 -0700

    requirements

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ queries the Reddit API and returns the number of subscribers """
+
+
 import requests
 
 
@@ -16,9 +18,9 @@ def number_of_subscribers(subreddit):
     """
     headers = {'User-Agent': 'linux:0x16.api.advanced'}
     response = requests.get('https://www.reddit.com/r/{}/about.json'
-                            .format(subreddit), headers=headers)
+                            .format(subreddit), headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return response.json()['data']['subscribers']
+        return response.json().get('data').get('subscribers')
     else:
         return 0
